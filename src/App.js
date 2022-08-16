bdc10dc05840334ae98b1eca3380c842c684737b
jason <jasperchibudike766@gmail.com>
2022-08-16 22:50:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 10
         </a>
       </header>
     </div>
